a76457cfbd280f828f588829147b87145659546f
Diego <108770121+Diego-Aco@users.noreply.github.com>
2026-04-01 08:54:27 -0500

    10 leapyears complete

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,30 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //Leap years are years divisible by four (like 1984 and 2004). 
+    // However, years divisible by 100 are not leap years (such as 1800 and 1900) 
+    // unless they are divisible by 400 (like 1600 and 2000, 
+    // which were in fact leap years). (Yes, it's all pretty confusing)
 
+    //year must be divisible by four but not 100, unless divisible by 400)
+
+    if (year%400===0) {
+        return true;
+    }
+
+    if (year%4===0 && year%100!==0) {
+        return true;
+    }
+    return false;
+
+    // if (year%4===0) {
+    //     if (year%100===0) {
+    //         if (year%400===0) {
+    //             return true;
+    //         }
+    //         return false;
+    //     }
+    //     return true;
+    // }
+    // return false;
 };
 
 // Do not edit below this line
---
+++ foundations/10_leapYears/main.js
@@ -0,0 +1,11 @@
+const leapYears = require("./leapYears.js")
+
+console.log(`2000 is ${leapYears(2000)}`);
+
+console.log(`1985 is ${leapYears(1985)}`);
+
+console.log(`1600 is ${leapYears(1600)}`);
+
+console.log(`1700 is ${leapYears(1700)}`);
+
+console.log(`1704 is ${leapYears(1704)}`);
