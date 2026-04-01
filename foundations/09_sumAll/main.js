1c15534c22d6e38e0f1193f1c223cb2a6b5da7f3
Diego <108770121+Diego-Aco@users.noreply.github.com>
2026-04-01 08:24:38 -0500

    09sumall complete

---
+++ foundations/09_sumAll/main.js
@@ -0,0 +1,9 @@
+const sumAll = require("./sumAll");
+let a= sumAll(1,4);
+console.log(a);
+
+let b = sumAll(4,1);
+console.log(b);
+
+console.log(sumAll(2,4));
+
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a)
+        || !Number.isInteger(b)
+        || a < 0
+        || b < 0) {
+        return "ERROR";
+    }
 
+    let lowBound = Math.min(a,b);
+    let highBound = Math.max(a,b);
+
+    let sum = 0;
+    for (let i=lowBound; i<=highBound; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
